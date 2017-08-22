80ecc2e8a42c0cdad6a43997bcd87bf5ed3cfaa7
woshicheyy@sina.com <woshicheyy@sina.com>
2017-08-22 17:32:35 +0800

    增加动态权限

--- app/src/main/java/com/cheyyy/zxing/MainActivity.java
+++ app/src/main/java/com/cheyyy/zxing/MainActivity.java
@@ -1,20 +1,27 @@
 package com.cheyyy.zxing;
 
+import android.Manifest;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.cheyyy.zxing.zxing.android.CaptureActivity;
-import com.tbruyelle.rxpermissions.RxPermissions;
+import com.tbruyelle.rxpermissions2.Permission;
+import com.tbruyelle.rxpermissions2.RxPermissions;
+
+import io.reactivex.functions.Consumer;
+
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
     private TextView tv_scanning_result;
     private EditText et_erweima_content;
     private ImageView result_img;
@@ -39,7 +46,36 @@ public class MainActivity extends AppCompatActivity {
         et_erweima_content = (EditText) findViewById(R.id.et_erweima_content);
         result_img = (ImageView) findViewById(R.id.result_img);
 
+        rxPermissions.requestEach(Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_CALENDAR,
+                Manifest.permission.READ_CALL_LOG,
+                Manifest.permission.READ_CONTACTS,
+                Manifest.permission.READ_PHONE_STATE,
+                Manifest.permission.READ_SMS,
+                Manifest.permission.RECORD_AUDIO,
+                Manifest.permission.CAMERA,
+                Manifest.permission.CALL_PHONE,
+                Manifest.permission.SEND_SMS)
+                .subscribe(new Consumer<Permission>() {
+                    @Override
+                    public void accept(Permission permission) throws Exception {
+                        if (permission.granted) {
+                            // 用户已经同意该权限
+                            Log.d(TAG, permission.name + " is granted.");
+                        } else if (permission.shouldShowRequestPermissionRationale) {
+                            // 用户拒绝了该权限，没有选中『不再询问』（Never ask again）,那么下次再次启动时，还会提示请求权限的对话框
+                            Log.d(TAG, permission.name + " is denied. More info should be provided.");
+                        } else {
+                            // 用户拒绝了该权限，并且选中『不再询问』
+                            Log.d(TAG, permission.name + " is denied.");
+                        }
+                    }
+                });
+
+
     }
+
 
 
     //点击跳转到扫描界面
